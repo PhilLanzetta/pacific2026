c2508338e99bf7d6dc81479e2262db09c1279bd8
PhilLanzetta <71086048+PhilLanzetta@users.noreply.github.com>
2026-01-13 16:18:41 -0500

    updates

--- src/pages/journal.js
+++ src/pages/journal.js
@@ -1,19 +1,58 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Layout from '../components/layout'
 import { graphql } from 'gatsby'
 import JournalTile from '../components/journalTile'
 
 const Journal = ({ location, data }) => {
-  const { tiles } = data.contentfulJournalLandingPage
+  const [activeFilters, setActiveFilters] = useState([])
+  const [tiles, setTiles] = useState(data.contentfulJournalLandingPage.tiles)
   const filters = [
-    'BRANDING',
-    'FILMS',
-    'ART',
-    'BOOKS',
-    'WRITING',
-    'SUNDAY READING',
-    'TECHNOLOGY',
+    'Branding',
+    'Films',
+    'Art',
+    'Books',
+    'Writing',
+    'Sunday Reading',
+    'Technology',
   ]
+
+  function onlyUnique(value, index, array) {
+    return array.indexOf(value) === index
+  }
+
+  const handleFilterClick = (item) => {
+    if (activeFilters.includes(item)) {
+      const newFilters = activeFilters.filter((filter) => filter !== item)
+      setActiveFilters(newFilters)
+    } else {
+      setActiveFilters([...activeFilters, item])
+    }
+  }
+
+  const filterByType = (array) => {
+    return [
+      ...array,
+      activeFilters
+        .map((term) => array.filter((item) => item.category.includes(term)))
+        .reduce((a, b) => a.concat(b), []),
+    ]
+  }
+
+  const handleFilter = () => {
+    let result = data.contentfulJournalLandingPage.tiles
+    if (activeFilters.length) {
+      result = filterByType(result)
+      result = result
+        .filter((item) => item.length)
+        .reduce((a, b) => a.concat(b), [])
+        .filter(onlyUnique)
+    }
+    setTiles(result)
+  }
+
+  useEffect(() => {
+    handleFilter()
+  }, [activeFilters])
 
   return (
     <Layout location={location}>
@@ -26,7 +65,17 @@ const Journal = ({ location, data }) => {
         <div className='journal-filter'>
           <button className='filter-btn-header'>Filter:</button>
           {filters.map((item, index) => (
-            <button key={index} className='filter-btn'>
+            <button
+              key={index}
+              className={`filter-btn ${
+                activeFilters.length > 0
+                  ? activeFilters.includes(item)
+                    ? 'active-filter'
+                    : 'non-active-filter'
+                  : 'active-filter'
+              }`}
+              onClick={() => handleFilterClick(item)}
+            >
               {item}
             </button>
           ))}
