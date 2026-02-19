191181b7a493467e9bbf7c91061d2612877f6626
PhilLanzetta <71086048+PhilLanzetta@users.noreply.github.com>
2026-02-19 10:00:15 -0500

    leadership

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,20 +1,101 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { graphql } from 'gatsby'
 import BodyText from '../components/bodyText'
 import ColumnModule from '../components/columnModule'
 import MailchimpSubscribe from 'react-mailchimp-subscribe'
 import CustomForm from '../components/customForm'
 import Seo from '../components/seo'
+import { GatsbyImage } from 'gatsby-plugin-image'
+import { AnimatePresence, motion } from 'framer-motion'
 
 const About = ({ data, location }) => {
+  const [lizExpand, setLizExpand] = useState(false)
+  const [adamExpand, setAdamExpand] = useState(false)
   const postUrl = process.env.GATSBY_MAIL_KEY
   const { content } = data.contentfulAboutPageRework
   const mainContent = content.slice(0, -1)
   const newsletter = content.pop()
+  const { lizBio, adamBio, lizHeadshot, adamHeadshot } =
+    data.contentfulAboutPage
+
+  const adamBioArray = adamBio.childMarkdownRemark.html.split('</p>')
+  const adamFirst = adamBioArray[0]
+  const adamRest = adamBioArray.slice(1).join()
+
+  const lizBioArray = lizBio.childMarkdownRemark.html.split('</p>')
+  const lizFirst = lizBioArray[0]
+  const lizRest = lizBioArray.slice(1).join()
+
   return (
     <div className='about-page-container'>
       <div className='about-page-padding-top'></div>
-      {mainContent.map((item) => {
+      {mainContent.slice(0, 6).map((item) => {
+        if (item.bodyTextId) {
+          return <BodyText content={item} key={item.bodyTextId}></BodyText>
+        } else if (item.columnId) {
+          return (
+            <ColumnModule key={item.columnId} content={item}></ColumnModule>
+          )
+        } else {
+          return <div>Unknown Content</div>
+        }
+      })}
+      <div className='outer-column-padding border-bottom'>
+        <h2>Leadership</h2>
+        <div className='leadership-container sans-serif body-text'>
+          <div>
+            <GatsbyImage
+              image={lizHeadshot.gatsbyImageData}
+              alt={lizHeadshot.description}
+            ></GatsbyImage>
+          </div>
+          <div>
+            <p className='upper'>Elizabeth Karp-Evans</p>
+            <div dangerouslySetInnerHTML={{ __html: lizFirst }}></div>
+            <AnimatePresence>
+              {lizExpand && (
+                <motion.div
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  exit={{ opacity: 0 }}
+                  dangerouslySetInnerHTML={{ __html: lizRest }}
+                ></motion.div>
+              )}
+            </AnimatePresence>
+            <button onClick={() => setLizExpand(!lizExpand)}>
+              <p>
+                {lizExpand ? '-' : '+'} READ {lizExpand ? 'LESS' : 'MORE'}
+              </p>
+            </button>
+          </div>
+          <div>
+            <GatsbyImage
+              image={adamHeadshot.gatsbyImageData}
+              alt={adamHeadshot.description}
+            ></GatsbyImage>
+          </div>
+          <div>
+            <p className='upper'>Adam Turnbull</p>
+            <div dangerouslySetInnerHTML={{ __html: adamFirst }}></div>
+            <AnimatePresence>
+              {adamExpand && (
+                <motion.div
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  exit={{ opacity: 0 }}
+                  dangerouslySetInnerHTML={{ __html: adamRest }}
+                ></motion.div>
+              )}
+            </AnimatePresence>
+            <button onClick={() => setAdamExpand(!adamExpand)}>
+              <p>
+                {adamExpand ? '-' : '+'} READ {adamExpand ? 'LESS' : 'MORE'}
+              </p>
+            </button>
+          </div>
+        </div>
+      </div>
+      {mainContent.slice(6).map((item) => {
         if (item.bodyTextId) {
           return <BodyText content={item} key={item.bodyTextId}></BodyText>
         } else if (item.columnId) {
@@ -103,6 +184,26 @@ export const query = graphql`
           wrapReverse
           borderPadding
         }
+      }
+    }
+    contentfulAboutPage(title: { eq: "Studio About Page" }) {
+      lizBio {
+        childMarkdownRemark {
+          html
+        }
+      }
+      adamBio {
+        childMarkdownRemark {
+          html
+        }
+      }
+      adamHeadshot {
+        gatsbyImageData
+        description
+      }
+      lizHeadshot {
+        gatsbyImageData
+        description
       }
     }
   }
