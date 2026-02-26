e7401e38a984215cb2a294b2b3c803b8dd61c41a
PhilLanzetta <71086048+PhilLanzetta@users.noreply.github.com>
2026-02-26 14:15:30 -0500

    fonts efficiency

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,8 @@
 import 'react-h5-audio-player/lib/styles.css'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
+import '@fontsource-variable/roboto-mono'
+import '@fontsource-variable/roboto'
 import './src/css/main.css'
 
 import CombinedProvider from './src/context/CombinedProvider'
